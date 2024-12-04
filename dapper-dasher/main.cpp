f3d347585ab801e1a4f35c1898600bd214264d98
Lacey Gruwell (Sikes) <lacey_sikes@my.cuesta.edu>
2024-12-04 01:36:48 -0800

    adding some jump logic with acceleration, gravity, and ground check

--- dapper-dasher/main.cpp
+++ dapper-dasher/main.cpp
@@ -8,11 +8,21 @@
 int main()
 {
     // window dimensions
-    const int width = 512;      // const variable will not change in the program
-    const int height = 380;     // I bet these have to do with the paralaxing image sizes
+    const int windowWidth = 512;      // const variable will not change in the program
+    const int windowHeight = 380;     // I bet these have to do with the paralaxing image sizes
     const char * windowTitle = "Lacey's Dapper Dasher Game";
     // initialize the window
-    InitWindow(width, height, windowTitle);
+    InitWindow(windowWidth, windowHeight, windowTitle);
+
+    // gravity, pixels/frame per frame
+    const int gravity{1};
+
+    // rectangle dimensions
+    const int rect_w{50};
+    const int rect_h{80};
+
+    int posY(windowHeight - rect_h);
+    int velocity{0};
 
     bool collision_with_enemy{};
 
@@ -24,11 +34,40 @@ int main()
         
         if ( collision_with_enemy )
         {
-            DrawText("Game Over!", width/2, height/2, 20, RED);
+            DrawText("Game Over!", windowWidth/2, windowHeight/2, 20, RED);
         }
         else
         {
             // Game Logic Start
+
+            // perform ground check
+            if ( posY >= (windowHeight - rect_h) )
+            {
+                // rectangle is on the ground
+                velocity = 0;
+            }
+            else
+            {
+                // rectangle is in the air, apply gravity
+                velocity += gravity;
+            }
+
+            // adding jump with SPACE logic
+            if ( IsKeyPressed(KEY_SPACE) )
+            {
+                // add to the velocity
+                velocity -= 10;
+            }
+            /*else
+            {
+                velocity = 0;
+            }*/
+
+
+            // update position
+            posY += velocity;
+
+            DrawRectangle(windowWidth/2, posY, rect_w, rect_h, BLUE);
 
             DrawText("Level: 1", 10, 10, 20, BLUE);
 
