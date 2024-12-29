b050c3ce952dea961169c63932378a41be597378
Lacey Gruwell (Sikes) <lacey_sikes@my.cuesta.edu>
2024-12-28 16:47:50 -0800

    Added animation to the character
    idle, running, left and right direction

--- topdown/main.cpp
+++ topdown/main.cpp
@@ -24,10 +24,20 @@ int main()
 
     // player
     Texture2D player = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D player_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D player_run = LoadTexture("characters/knight_run_spritesheet.png");
     Vector2 playerPosition{
         (float)windowWidth/2.0f - 4.0f * (0.5f * (float)player.width/6.0f),    // c-style cast
         (float)windowHeight/2.0f - 4.0f * (0.5f * (float)player.height)
     };
+    // 1: facing right, -1: facing left
+    float rightLeft{1.f};
+
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f/12.f};   // update 12 times per second
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -43,16 +53,30 @@ int main()
         if (Vector2Length(direction) != 0.0)
         {
             // set mapPosition = mapPosition - direction
-            
             mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f; // ternary operator
+            player = player_run;// update player idleness
+        }
+        else
+        {
+            player = player_idle;
         }
 
 
         // draw the map with DrawTextureEx()
         DrawTextureEx(map, mapPosition, 0.0, 1.0, WHITE);
 
+        // update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.f;
+            if (frame > maxFrames) frame = 0;
+        }
+
         // draw the player
-        Rectangle source{0.f, 0.f, (float)player.width/6.f, (float)player.height};
+        Rectangle source{frame * (float)player.width/6.f, 0.f, rightLeft * (float)player.width/6.f, (float)player.height};
         Rectangle dest{playerPosition.x, playerPosition.y, 2.0f * (float)player.width/6.0f, 2.0f * (float)player.height};
         Vector2 origin{};
         DrawTexturePro(player, source, dest, origin, 0.f, WHITE);
@@ -64,6 +88,8 @@ int main()
 
     // unload the map texture
     UnloadTexture(map);
+    UnloadTexture(player_idle);
+    UnloadTexture(player_run);
 
 
 }
